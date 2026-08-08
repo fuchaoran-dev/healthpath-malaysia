bf47a6e857d2fc233d8d5d0f8d9b76c2d5ea8901
WoonTaek <wbai0002@student.monash.edu>
2026-08-08 16:54:29 +0800

    file construct

---
+++ src/constants/options.js
@@ -0,0 +1,48 @@
+export const AGE_GROUPS = [
+  "40-44",
+  "45-49",
+  "50-54",
+  "55-60",
+];
+
+export const GENDERS = [
+  "male",
+  "female",
+  "prefer_not_to_say",
+];
+
+export const FAMILY_HISTORY = [
+  "heart_disease",
+  "diabetes",
+  "cancer",
+  "stroke",
+  "none",
+  "unsure",
+];
+
+export const STATES = [
+  "Johor",
+  "Kedah",
+  "Kelantan",
+  "Melaka",
+  "Negeri Sembilan",
+  "Pahang",
+  "Perak",
+  "Perlis",
+  "Pulau Pinang",
+  "Sabah",
+  "Sarawak",
+  "Selangor",
+  "Terengganu",
+  "W.P. Kuala Lumpur",
+  "W.P. Labuan",
+  "W.P. Putrajaya",
+];
+
+export const STORAGE = {
+  sessionId: "healthpath-session-id",
+  consent: "healthpath-consent",
+  profile: "healthpath-profile",
+  result: "healthpath-result",
+  goals: "healthpath-goals",
+};
